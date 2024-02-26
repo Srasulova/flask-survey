7a283a4c5d848b153d6dec6e315db4580d7f2d8b
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-02-26 15:44:44 -0500

    add logic to handle question and answer routes

--- app.py
+++ app.py
@@ -17,3 +17,20 @@ def home_page():
     instructions = survey.instructions
     return render_template("survey_start.html", title = title, instructions = instructions )
 
+@app.route("/start", methods=["POST"])
+def start_survey():
+    return redirect('/questions/0')
+
+
+@app.route('/questions/<int:qID>')
+def show_question(qID):
+    question = survey.questions[qID]
+    return render_template("question.html", question = question)
+
+@app.route('/answer', methods=["POST"])
+def show_answers():
+    choice = request.form['answer']
+    responses.append(choice)
+    print(responses)
+    return render_template("/answers.html", responses = responses)
+
